b5074a4dc2ddfbdc673fe67f77c18e9fdf13596e
mingrow <nikitosvozh@gmail.com>
2024-08-22 05:51:09 +0300

    2. HQL [#6874 #509971]

--- src/main/java/ru/job4j/repository/UserRepository.java
+++ src/main/java/ru/job4j/repository/UserRepository.java
@@ -15,16 +15,22 @@ public class UserRepository {
     private final SessionFactory sessionFactory;
 
     public User create(User user) {
-        try (Session session = sessionFactory.openSession()) {
+        Session session = sessionFactory.openSession();
+        try  {
             session.beginTransaction();
             session.save(user);
             session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
         return user;
     }
 
     public void update(User user) {
-        try (Session session = sessionFactory.openSession()) {
+        Session session = sessionFactory.openSession();
+        try  {
             session.beginTransaction();
             session.createQuery(
                     "update User u set u.password = :password where u.id = :id")
@@ -32,62 +38,91 @@ public class UserRepository {
                     .setParameter("id", user.getId())
                     .executeUpdate();
             session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
     }
 
     public void delete(int id) {
-        try (Session session = sessionFactory.openSession()) {
+        Session session = sessionFactory.openSession();
+        try {
             session.beginTransaction();
             session.createQuery("delete from User u where u.id = :id")
                     .setParameter("id", id)
                     .executeUpdate();
             session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
     }
 
     public List<User> findAllOrderById() {
         List<User> users = new ArrayList<>();
-        try (Session session = sessionFactory.openSession()) {
+        Session session = sessionFactory.openSession();
+        try {
             session.beginTransaction();
             Query<User> query = session.createQuery("from User u order by u.id", User.class);
             users = query.list();
             session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
         return users;
     }
 
     public Optional<User> findById(int id) {
         Optional<User> user = Optional.empty();
-        try (Session session = sessionFactory.openSession()) {
+        Session session = sessionFactory.openSession();
+        try {
             session.beginTransaction();
             Query<User> query = session.createQuery("from User u where u.id = :id", User.class);
             query.setParameter("id", id);
-            user = Optional.ofNullable(query.uniqueResult());
+            user = query.uniqueResultOptional();
             session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
         return user;
     }
 
     public List<User> findByLikeLogin(String key) {
         List<User> users = new ArrayList<>();
-        try (Session session = sessionFactory.openSession()) {
+        Session session = sessionFactory.openSession();
+        try {
             session.beginTransaction();
             Query<User> query = session.createQuery("from User u where u.login like :likeKey ", User.class);
             query.setParameter("likeKey", "%" + key + "%");
             users = query.list();
             session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
         return users;
     }
 
     public Optional<User> findByLogin(String login) {
         Optional<User> rsl = Optional.empty();
-        try (Session session = sessionFactory.openSession()) {
+        Session session = sessionFactory.openSession();
+        try {
             session.beginTransaction();
             Query<User> query = session.createQuery("from User u where u.login = :login", User.class);
             query.setParameter("login", login);
-            rsl = Optional.ofNullable(query.uniqueResult());
+            rsl = query.uniqueResultOptional();
             session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
         return rsl;
     }
